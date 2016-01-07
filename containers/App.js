1c1c8dc2bb702cd717a6e24c5793163a43842612
Paweł Walczewski <pawel.walczewski@unity.pl>
2016-01-07 16:01:10 +0100

    routing update

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { pushState } from 'redux-router'
-import Viewer3D from '../components/Viewer3D'
 import Header from '../components/header/Header'
 import { resetErrorMessage } from '../actions'
 import './app.scss';
@@ -44,16 +43,11 @@ class App extends Component {
   render() {
     const { children, inputValue } = this.props;
 
-    //additional, not required scene options
-    let sceneOptions = {
-      width : 700,
-      height : 500
-    }
 
     return (
       <div>
         <Header></Header>
-        <Viewer3D modelSource="/cube.json" sceneOptions={sceneOptions} showControls="true"  />
+       
         <hr />
         {this.renderErrorMessage()}
         {children}
--- routes.js
+++ routes.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Route } from 'react-router'
+import { Route, IndexRoute } from 'react-router'
 import App from './containers/App'
 import UserPage from './containers/UserPage'
 import RepoPage from './containers/RepoPage'
 import AboutPage from './containers/AboutPage'
-
+import RenderPage from './containers/RenderPage'
 export default (
   <Route path="/" component={App}>
+  	<IndexRoute component={RenderPage}/>
   	<Route path="/about"
            component={AboutPage} />
     <Route path="/:login/:name"
